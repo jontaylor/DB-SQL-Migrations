7674ca07f15b12106ec339940d9679711344fccf
Jon Taylor <jon@stackhaus.com>
2013-01-09 12:21:58 +0000

    Remove the sleep

--- lib/DB/SQL/Migrations.pm
+++ lib/DB/SQL/Migrations.pm
@@ -6,7 +6,7 @@ use File::Basename;
 use DBIx::MultiStatementDo;
 use File::Slurp;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 has [qw( dbh migrations_directory  )];
 has schema_migrations_table => sub { 'schema_migrations' };
