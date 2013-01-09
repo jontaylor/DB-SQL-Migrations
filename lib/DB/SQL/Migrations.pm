aacd867f3222522d9ec7031f445a203b69d333d2
Jon Taylor <jon@stackhaus.com>
2013-01-09 12:21:45 +0000

    Remove the sleep

--- lib/DB/SQL/Migrations.pm
+++ lib/DB/SQL/Migrations.pm
@@ -50,8 +50,6 @@ sub apply {
   my @pending_migrations = $self->_pending_migrations;
 
   if(scalar(@pending_migrations)) {
-    print "Proceeding in 2 seconds, Ctrl-C to abort\n";
-    sleep(2);
 
     foreach my $migration(@pending_migrations) {
       $self->_apply_migration($migration);
